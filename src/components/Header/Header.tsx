208780b0c87191374ac720062c56cc2c4140ef43
“Abhinav <vabhinav368@gmail.com>
2023-07-17 20:14:35 +0530

    fixing logo positioning

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,23 +10,23 @@ const Header: React.FC<IHeaderProps> = (props) => {
   const router = useRouter();
   return (
     <div>
-      <div className="flex items-center justify-between px-[24px] bg-[#444] py-[16px] sticky w-full">
+      <div className="flex items-center justify-center px-[24px] bg-[#444] py-[16px] sticky w-full">
         <div
           onClick={() => {
             router.push("/");
           }}
-          className="cursor-pointer"
+          className="cursor-pointer text-[24px]"
         >
           Your Tour
         </div>
-        <div className="hidden md:flex">
-          {/* <ul className="flex items-center justify-between gap-x-6">
+        {/* <div className="hidden md:flex"> */}
+        {/* <ul className="flex items-center justify-between gap-x-6">
             <li>Login</li>
             <li className="border-[1px] border-solid rounded-[16px] p-[8px]">
               SignUp
             </li>
           </ul> */}
-        </div>
+        {/* </div> */}
         {/* {!props.open && (
           <div className=" flex md:hidden" onClick={() => props.setOpen(true)}>
             <img
